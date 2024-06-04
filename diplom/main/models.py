83f446a2bb5e5e51490ce41ff54cdfe2f57d0b58
Danil Galeev <galoffdan@gmail.com>
2024-06-04 18:21:15 +0300

    + course model

--- diplom/main/models.py
+++ diplom/main/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
+
 # Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=255, null=False, blank=False, verbose_name="название")
+    logo = models.ImageField(verbose_name="логотип", null=True, blank=True)
+    users = models.ManyToManyField(get_user_model(), related_name="courses")
+
+    class Meta:
+        verbose_name = "курс"
+        verbose_name_plural = "курсы"
