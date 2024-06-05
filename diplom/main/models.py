080cd896f420068ebef0a78a7b44c51b2ae531f7
Danil Galeev <galoffdan@gmail.com>
2024-06-05 13:03:45 +0300

    + participation application model

--- diplom/main/models.py
+++ diplom/main/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+from users.models import User
 
 
 # Create your models here.
@@ -11,3 +14,17 @@ class Course(models.Model):
     class Meta:
         verbose_name = "курс"
         verbose_name_plural = "курсы"
+
+
+class ParticipationApplication(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.RESTRICT, related_name="participation_applications")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="participation_applications")
+    price = models.DecimalField(max_digits=11, decimal_places=2)
+    phone_number = PhoneNumberField(blank=False, null=False)
+    time = models.TimeField(blank=False, null=False)
+    date = models.DateField(blank=False, null=False)
+    wishes = models.TextField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = "заявка на участие"
+        verbose_name_plural = "заявки на участие"
